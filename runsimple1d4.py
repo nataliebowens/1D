bae1cd856ea2d388516901509b50c3e9a3d56bec
Natalie Bowens <nataliebowens@gmail.com>
2012-06-08 11:35:34 -0700

    Better code for runsimple1d4.py

--- runsimple1d4.py
+++ runsimple1d4.py
@@ -2,6 +2,10 @@
 
 # Independent parameters controlling operation of simple1d3
 
+# This code reloads the pre_data(data at steady state), it 
+# has you change something and then it saves the changed data
+# file under another name that depends on the changed variable.
+
 from pylab import *
 import pdb
 import time
@@ -9,109 +13,129 @@ import pickle
 import simple1d3
 reload(simple1d3)
 
+print "Reloading input data"
 
-ans = raw_input("Reloading input data")
+f = open('pre_simple1d3.dat', 'r')
+Fliq0 = pickle.load(f)
+Nice0 = pickle.load(f)
+x = pickle.load(f)
+diffperdt= pickle.load(f)                
+rainperdt_terr = pickle.load(f)
+rainperdt_edge = pickle.load(f)
+Fliqmax = pickle.load (f)
+f.close()
 
-if ans == '':
-    f = open('pre_simple1d3.dat', 'r')
-    Fliq0 = pickle.load(f)
-    Nice0 = pickle.load(f)
-    x = pickle.load(f)
-    diffperdt= pickle.load(f)                 #before it said = pickle.load(f)
-    rainperdt_terr = pickle.load(f)
-    rainperdt_edge = pickle.load(f)
-    Fliqmax = pickle.load (f)
-    f.close()
-    #diffperdt=0.01
-    Ntimes =1000
-    #Fliq0 = Fliq0 + 3*exp(-(x-250)**2/(2*100^2))
-#I added this stuff to hopefully code for different pertibation    
-    xmax = 501
-    supersat = 0.04                #not sure if the following was used in this model
-    supersatpfactor = 0.9
-    alpha_terr = 0.5
-    alpha_edge = 1.0
-    supersatp = supersat * supersatpfactor
-    c = (supersat - supersatp) / xmax ** 2
-    rainperdt = x ** 2 * c + supersatp
-    rainperdt_terr = rainperdt * alpha_terr
-    rainperdt_edge = rainperdt * alpha_edge
+ntimes= raw_input("How many extra steps:")
+Ntimes = int(ntimes)
+#Fliq0 = Fliq0 + 3*exp(-(x-250)**2/(2*100^2))
+
+supersat = 0.04                
+supersatpfactor = 0.9
+alpha_terr = 0.2
+alpha_edge = 1.0
+diffperdt=0.05
+an= raw_input("1.supersat \n2.supersatpfactor \n3.alpha_terr \n4.alpha_edge \n5.diffperdt \n# of variable you would like to change:")
+ans= int(an)
+if ans== 1:
+    var= raw_input("Change from "+ supersat +"to:")
+    supersat= float(var)
+elif ans ==2:
+    var= raw_input("Change from "+ supersatpfactor +"to:")
+    supersatpfactor= float(var)
+elif ans ==3:
+    var= raw_input("Change from "+ alpha_terr +"to:")
+    alpha_terr= float(var)
+elif ans ==4:
+    var= raw_input("Change from "+ alpha_edge +"to:")
+    alpha_edge= float(var)
+elif ans ==5:
+    var= raw_input("Change from "+ diffperdt +"to:")
+    diffperdt= float(var)
+else :
+    print "Nothing was changed! Label saved file 'erase'"
+
+
+supersatp = supersat * supersatpfactor
+c = (supersat - supersatp) / xmax ** 2
+rainperdt = x ** 2 * c + supersatp
+rainperdt_terr = rainperdt * alpha_terr
+rainperdt_edge = rainperdt * alpha_edge
     
-else:
-
-    Nx = 500 #Changed this to half
-    xmax = 501
-    Fliqstart = 0.1
-    Ntimes = 30000
-    diffperdt = 0.05
-    supersat = 0.04
-    supersatpfactor = 0.9
-    alpha_terr = 0.2
-    alpha_edge = 1.0
-    Fliqmax = 1.5
-    x = linspace(0, xmax, Nx)
-    Fliq0 = ones(size(x)) * Fliqstart
-    Nice0 = zeros(size(x))
-    supersatp = supersat * supersatpfactor
-    c = (supersat - supersatp) / xmax ** 2
-    rainperdt = x ** 2 * c + supersatp
-    rainperdt_terr = rainperdt * alpha_terr
-    rainperdt_edge = rainperdt * alpha_edge
+#else:
+#
+#    Nx = 500 #Changed this to half
+#    xmax = 501
+#    Fliqstart = 0.1
+#    Ntimes = 30000
+#    diffperdt = 0.05
+#    supersat = 0.04
+#    supersatpfactor = 0.9
+#    alpha_terr = 0.2
+#    alpha_edge = 1.0
+#    Fliqmax = 1.5
+#    x = linspace(0, xmax, Nx)
+#    Fliq0 = ones(size(x)) * Fliqstart
+#    Nice0 = zeros(size(x))
+#    supersatp = supersat * supersatpfactor
+#    c = (supersat - supersatp) / xmax ** 2
+#    rainperdt = x ** 2 * c + supersatp
+#    rainperdt_terr = rainperdt * alpha_terr
+#    rainperdt_edge = rainperdt * alpha_edge
 
 
     ##Ask: "Default OR input parameters?"
-    inParm= raw_input("Default or input parameters? \n(d or i)")
+#    inParm= raw_input("Default or input parameters? \n(d or i)")
     
     ##Input the parameters. If press enter, then they will be defaulted.
-    if inParm== 'i':
+#   if inParm== 'i':
 #        nx= raw_input("Nx(int):")
 #        if nx== '':
 #            pass
 #        else:
 #            Nx= int(nx)
 #        print Nx
-        
-        
-        ntimes= raw_input("Ntimes(int):")
-        if ntimes== '':
-            pass
-        else:
-            Ntimes= int(ntimes)
-        print Ntimes
-        
-        
-        raindt= raw_input("Diffusitivity(float):")
-        if raindt== '':
-            pass
-        else:
-            diffperdt = float(raindt)
-        print diffperdt
-        
-        
-        supasat= raw_input("Supersat(float):")
-        if raindt== '':
-            pass
-        else:
-            diffperdt = float(raindt)
-        print diffperdt
-        
-        
-        deltasupsat= raw_input("Difference in supersat btw middle & edge(between 0-1):")
-        if deltasupsat== '':
-            pass
-        else:
-            deltasupsat = float(supersatpfactor)
-        print supersatpfactor
-        
-        
-        alpha_terr0= raw_input("Alpha terrace(btw 0-1): ")
-        if alpha_terr0== '':
-            pass
-        else:
-            alpha_terr= float(alpha_terr0)
-        print alpha_terr
-        
-            
+#        
+#        
+#        ntimes= raw_input("Ntimes(int):")
+#        if ntimes== '':
+#            pass
+#        else:
+#            Ntimes= int(ntimes)
+#        print Ntimes
+#        
+#        
+#        raindt= raw_input("Diffusitivity(float):")
+#        if raindt== '':
+#            pass
+#        else:
+#            diffperdt = float(raindt)
+#        print diffperdt
+#        
+#        
+#        supasat= raw_input("Supersat(float):")
+#        if raindt== '':
+#            pass
+#        else:
+#            diffperdt = float(raindt)
+#        print diffperdt
+#        
+#        
+#        deltasupsat= raw_input("Difference in supersat btw middle & edge(between 0-1):")
+#        if deltasupsat== '':
+#            pass
+#        else:
+#            deltasupsat = float(supersatpfactor)
+#        print supersatpfactor
+#        
+#        
+#        alpha_terr0= raw_input("Alpha terrace(btw 0-1): ")
+#        if alpha_terr0== '':
+#            pass
+#        else:
+#            alpha_terr= float(alpha_terr0)
+#        print alpha_terr
+#        
+#            
 
 
 # Call to simple1d3
