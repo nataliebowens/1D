bbaadb02a878f84af5f14b5c090126e88b36d79c
Natalie Bowens <nataliebowens@gmail.com>
2012-06-10 13:01:22 -0700

    hahahha, still not working

--- plotnFxn.py
+++ plotnFxn.py
@@ -12,22 +12,8 @@ f = open('pre_simple1d3.dat', 'r')
 Fliq = pickle.load(f)
 Nice = pickle.load(f)
 x = pickle.load(f)
-diffperdt= pickle.load(f)                
-rainperdt_terr = pickle.load(f)
-rainperdt_edge = pickle.load(f)
-Fliqmax = pickle.load (f)
 f.close()
 print "loaded, now to dump!"
-# Save it
-f = open('d.dat', 'w')
-pickle.dump(Fliq, f)
-pickle.dump(Nice, f)
-pickle.dump(x, f)
-pickle.dump(diffperdt, f)
-pickle.dump(rainperdt_terr, f)
-pickle.dump(rainperdt_edge, f)
-pickle.dump(Fliqmax, f)
-f.close()
 
 # Plot it
 figurenumber = 1
@@ -40,4 +26,13 @@ stitle = 'Ntimes = '
 stitle += "string"
 stitle += ' 1d model'
 title(stitle)
+
+
+# Save it
+f = open('d.dat', 'w')
+pickle.dump(Fliq, f)
+pickle.dump(Nice, f)
+pickle.dump(x, f)
+f.close()
+
 
